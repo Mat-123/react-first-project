83b5d2802a674f534adafa260b5d4380fdbdd67f
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-03-11 17:21:36 +0100

    rotating dog logo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
         <ButtonComponent />
 
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://placedog.net/400"} className="App-logo " alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
